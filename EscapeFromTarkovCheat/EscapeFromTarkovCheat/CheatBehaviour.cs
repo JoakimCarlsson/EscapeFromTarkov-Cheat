726942530a3c88f9974318728cfb26874c700154
Carlsson <Carlsson@DESKTOP-PAIRRD8>
2020-02-24 17:58:40 +0100

    Initial Project

---
+++ EscapeFromTarkovCheat/EscapeFromTarkovCheat/CheatBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EscapeFromTarkovCheat
+{
+    public class CheatBehaviour : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void OnGUI()
+        {
+
+        }
+    }
+}
---
+++ EscapeFromTarkovCheat/EscapeFromTarkovCheat/Loader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace EscapeFromTarkovCheat
+{
+    public class Loader
+    {
+        public static GameObject HookObject
+        {
+            get
+            {
+                var result = GameObject.Find("Application (Main Client)");
+                if (result == null)
+                {
+                    result = new GameObject("Trainer");
+                    Object.DontDestroyOnLoad(result);
+                }
+                return result;
+            }
+        }
+
+        public static void Load()
+        {
+            HookObject.AddComponent<CheatBehaviour>();
+        }
+    }
+
+}
