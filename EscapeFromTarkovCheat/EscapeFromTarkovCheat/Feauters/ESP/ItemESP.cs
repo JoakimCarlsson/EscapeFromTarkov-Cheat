7a8bf908b1c399744cf1d3b18e8c6b7e42edd421
Carlsson <Carlsson@DESKTOP-14GSI35>
2020-03-02 20:40:30 +0000

    Small improvment too draw items performance

--- EscapeFromTarkovCheat/EscapeFromTarkovCheat/Feauters/ESP/ItemESP.cs
+++ EscapeFromTarkovCheat/EscapeFromTarkovCheat/Feauters/ESP/ItemESP.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Comfort.Common;
 using EFT;
@@ -11,11 +12,14 @@ namespace EscapeFromTarkovCheat.Feauters.ESP
     public class ItemESP : MonoBehaviour
     {
         private IEnumerable<LootItem> _lootItems;
+        private List<LootItem> _lootItemsToDraw;
         private void Start()
         {
-            InvokeRepeating(nameof(GetItems), 10f, 10f);
+            AllocConsoleHandler.Open();
+            _lootItemsToDraw = new List<LootItem>();
+            InvokeRepeating(nameof(GetAllItems), 10f, 10f);
+            InvokeRepeating(nameof(GetItemsToDraw), 10, 12f);
         }
-
         private void OnGUI()
         {
             if (Settings.DrawLootItems)
@@ -26,10 +30,10 @@ namespace EscapeFromTarkovCheat.Feauters.ESP
 
         public void DrawItemESP()
         {
-            if (_lootItems == null)
+            if (_lootItemsToDraw == null)
                 return;
 
-            foreach (LootItem lootItem in _lootItems)
+            foreach (LootItem lootItem in _lootItemsToDraw)
             {
                 if (lootItem == null)
                     continue;
@@ -39,41 +43,15 @@ namespace EscapeFromTarkovCheat.Feauters.ESP
 
                 if (boundingVector.z > 0.01 && distance <= Settings.DrawLootItemsDistance)
                 {
-                    if (lootItem.name.Contains("key") ||
-                        lootItem.name.Contains("usb") ||
-                        lootItem.name.Contains("alkali") ||
-                        lootItem.name.Contains("ophalmo") ||
-                        lootItem.name.Contains("gunpowder") ||
-                        lootItem.name.Contains("phone") ||
-                        lootItem.name.Contains("money") ||
-                        lootItem.name.Contains("document") ||
-                        lootItem.name.Contains("quest") ||
-                        lootItem.name.Contains("spark") ||
-                        lootItem.name.Contains("grizzly") ||
-                        lootItem.name.Contains("sv-98") ||
-                        lootItem.name.Contains("sv98") ||
-                        lootItem.name.Contains("rsas") ||
-                        lootItem.name.Contains("salewa") ||
-                        lootItem.name.Equals("bitcoin") ||
-                        lootItem.name.Contains("dvl") ||
-                        lootItem.name.Contains("m4a1") ||
-                        lootItem.name.Contains("roler") ||
-                        lootItem.name.Contains("chain") ||
-                        lootItem.name.Contains("wallet") ||
-                        lootItem.name.Contains("RSASS") ||
-                        lootItem.name.Contains("glock") ||
-                        lootItem.name.Contains("SA-58"))
-                    {
-                        string text = $"{lootItem.name} - [{(int)distance}]m";
+                    string text = $"{lootItem.name} - [{(int)distance}]m";
 
-                        GUI.color = Color.yellow;
-                        GUI.Label(new Rect(boundingVector.x - 50f, Screen.height - boundingVector.y, 100f, 50f), text);
-                    }
+                    GUI.color = Color.yellow;
+                    GUI.Label(new Rect(boundingVector.x - 50f, Screen.height - boundingVector.y, 100f, 50f), text);
                 }
             }
         }
 
-        public void GetItems()
+        private void GetAllItems()
         {
             GameWorld world = Singleton<GameWorld>.Instance;
 
@@ -81,6 +59,47 @@ namespace EscapeFromTarkovCheat.Feauters.ESP
             {
                 if (Settings.DrawLootItems)
                     _lootItems = FindObjectsOfType<LootItem>();
+            }
+        }
+
+        private void GetItemsToDraw()
+        {
+            if (_lootItems == null)
+                return;
+            
+            foreach (LootItem lootItem in _lootItems)
+            {
+                if (lootItem == null)
+                    continue;
+
+                if (lootItem.name.Contains("key") ||
+                    lootItem.name.Contains("usb") ||
+                    lootItem.name.Contains("alkali") ||
+                    lootItem.name.Contains("ophalmo") ||
+                    lootItem.name.Contains("gunpowder") ||
+                    lootItem.name.Contains("phone") ||
+                    lootItem.name.Contains("money") ||
+                    lootItem.name.Contains("document") ||
+                    lootItem.name.Contains("quest") ||
+                    lootItem.name.Contains("spark") ||
+                    lootItem.name.Contains("grizzly") ||
+                    lootItem.name.Contains("sv-98") ||
+                    lootItem.name.Contains("sv98") ||
+                    lootItem.name.Contains("rsas") ||
+                    lootItem.name.Contains("salewa") ||
+                    lootItem.name.Equals("bitcoin") ||
+                    lootItem.name.Contains("dvl") ||
+                    lootItem.name.Contains("m4a1") ||
+                    lootItem.name.Contains("roler") ||
+                    lootItem.name.Contains("chain") ||
+                    lootItem.name.Contains("wallet") ||
+                    lootItem.name.Contains("RSASS") ||
+                    lootItem.name.Contains("glock") ||
+                    lootItem.name.Contains("SA-58"))
+                {
+                    Console.WriteLine(lootItem.name);
+                    _lootItemsToDraw.Add(lootItem);
+                }
             }
         }
     }
